e319ab34379ff89f569c1b817503ef7c495304c1
Hicret Kuş <hicret.kus@hotmail.com>
2024-03-27 16:04:57 +0300

    Add project files.

---
+++ FisTaramaSistemi/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace FisTaramaSistemi
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string jsonFilePath = @"C:\Users\acer\Downloads\response.json";
+
+			string jsonText = File.ReadAllText(jsonFilePath);
+
+			JArray response = JArray.Parse(jsonText);
+
+			if (response != null && response.Count > 0)
+			{
+				var firstItem = response[0];
+
+				var firstVertex = firstItem["boundingPoly"]["vertices"][0];
+
+				int x = (int)firstVertex["x"];
+
+				int y = (int)firstVertex["y"];
+
+
+				string description = (string)firstItem["description"];
+
+				string formattedLine = $"{description}";
+
+				Console.WriteLine(formattedLine);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
