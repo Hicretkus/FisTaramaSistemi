6d889506f6169d8d9f7327d9a3f976e963f93834
Hicretkus <80815390+Hicretkus@users.noreply.github.com>
2024-03-27 22:35:08 +0300

    Update Program.cs

--- FisTaramaSistemi/Program.cs
+++ FisTaramaSistemi/Program.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq; // Newtonsoft.Json kütüphanesinden JArray gibi JSON işlemleri için gerekli nesneleri kullanabilmek için eklendi.
 
 namespace FisTaramaSistemi
 {
@@ -8,26 +8,26 @@ namespace FisTaramaSistemi
 		{
 			try
 			{
-				string jsonFilePath = @"C:\Users\acer\Downloads\response.json";
+				string jsonFilePath = @"C:\Users\acer\Downloads\response.json";  // jsonFilePath değişkeni dosyanın yolunu tutar.
 
 				
-				string jsonText = await File.ReadAllTextAsync(jsonFilePath);
+				string jsonText = await File.ReadAllTextAsync(jsonFilePath); // await File.ReadAllTextAsync(jsonFilePath) ifadesiyle dosyanın içeriği bir string olarak jsonText değişkenine atanır.
 
-				JArray response = JArray.Parse(jsonText);
+				JArray response = JArray.Parse(jsonText); // jsonText içindeki JSON verisi JArray.Parse metodu ile bir JArray nesnesine dönüştürülür. JSON verisinin bir dizi olarak işlenmesini sağlar.
 
-				if (response?.Count > 0) 
+				if (response?.Count > 0) // response JArray'indeki ilk öğeyi işler. 
 				{
-					var firstItem = response[0];
+					var firstItem = response[0]; // eğer dizi boş değilse, dizinin ilk öğesini (firstItem) alır.
 
-					var firstVertex = firstItem["boundingPoly"]?["vertices"]?[0];
+					var firstVertex = firstItem["boundingPoly"]?["vertices"]?[0]; // firstItem içindeki "boundingPoly" anahtarına ait nesnenin "vertices" dizisinin ilk elemanı (firstVertex) alınır.
 
-					int x = firstVertex?["x"]?.ToObject<int>() ?? 0; 
+					int x = firstVertex?["x"]?.ToObject<int>() ?? 0; // firstVertex içindeki x ve y koordinatları çıkarılır. Eğer bu değerler null ise varsayılan olarak 0 atanır.
 					int y = firstVertex?["y"]?.ToObject<int>() ?? 0; 
 
-					string description = (string)firstItem["description"];
+					string description = (string)firstItem["description"]; //  firstItem içindeki "description" anahtarına ait değer string olarak description değişkenine atanır.
 
 					
-					Console.WriteLine($"{description}");
+					Console.WriteLine($"{description}"); // description değişkenindeki değer konsola yazdırılır.
 				}
 			}
 			catch (Exception ex)
